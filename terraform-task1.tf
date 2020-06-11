466097de70375d9f7a3bf078feff2b26f2e408b0
Divyansh747 <divyansh.1999.dr@gmail.com>
2020-06-11 17:19:19 +0530

    add aws ec2 instance

--- terraform-task1.tf
+++ terraform-task1.tf
@@ -32,3 +32,22 @@ resource "aws_security_group" "ssh-http-1" {
   }
 }
 
+#aws instance
+resource "aws_instance" "aws-os-1" {
+  ami               = "ami-0447a12f28fddb066"
+  instance_type     = "t2.micro"
+  availability_zone = "ap-south-1a"
+  security_groups   = ["ssh-http"]
+  key_name          = "test-os-key"
+  user_data         = <<-EOF
+                       #!/bin/bash
+                       sudo yum install httpd -y
+                       sudo systemctl start httpd
+                       sudo systemctl enable httpd
+                       EOF  
+  tags = {
+    Name = "aws-os-1"
+  }
+}
+
+
