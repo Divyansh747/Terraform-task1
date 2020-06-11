aca415b54aae504c7fc0f4b7ac3a860000779628
Divyansh747 <divyansh.1999.dr@gmail.com>
2020-06-11 17:18:37 +0530

    add aws security group

--- terraform-task1.tf
+++ terraform-task1.tf
@@ -3,3 +3,32 @@ provider "aws" {
   region     = "ap-south-1"
   profile    = "mytest"
 }
+
+#security group
+resource "aws_security_group" "ssh-http-1" {
+  name        = "ssh-http"
+  description = "allow ssh and http"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
